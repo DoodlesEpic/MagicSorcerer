613e0546d700bea3145b0b29cf3cf6422ef157a7
Doodles <moraes.eduardo@proton.me>
2022-12-23 17:02:53 -0300

    Style the page using Bootstrap SCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,51 @@
-<main>
-	<h1>MTG Sorcerer</h1>
-	<p>
-		Welcome to Magic the Gathering Sorcerer, a website that uses AI to provide useful suggestions to
-		help you improve your Magic: The Gathering decklists. Whether you're a seasoned pro or a
-		newcomer to the game, our AI can help you identify combos, understand your deck's strengths and
-		weaknesses, and make informed decisions about which cards to add or remove. With Magic the
-		Gathering Sorcerer, you can easily get the advice you need to take your deck to the next level.
-		Our user-friendly interface and accessible language make it easy for any Magic player to benefit
-		from our AI's expert insights. So why wait? Try Magic the Gathering Sorcerer today and see what
-		our AI can do for you!
-	</p>
-	<ul>
-		<li>Ada: A state-of-the-art language model that can generate human-like text.</li>
-		<li>
-			Babbage: A language model that can answer questions and provide information on a wide range of
-			topics.
-		</li>
-		<li>
-			Curie: A language model that can generate text with a sense of curiosity and playfulness.
-		</li>
-		<li>Davinci: A language model that can perform translation and summarization tasks.</li>
-	</ul>
+<nav class="navbar sticky-top bg-body shadow-sm">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">MTG Sorcerer</a>
+	</div>
+</nav>
+<main class="container mt-5">
+	<section class="mb-3">
+		<p>
+			Welcome to Magic the Gathering Sorcerer. The website uses AI to provide suggestions to help
+			you improve your Magic: The Gathering decklists. Whether you're a seasoned pro or a newcomer
+			to the game, our AI can help you identify combos, understand your deck's strengths and
+			weaknesses, and make informed decisions about which cards to add or remove. Our user-friendly
+			interface and accessible language make it easy for any Magic player to benefit from our AI's
+			expert insights.
+		</p>
+		<ul>
+			<li>Ada: A state-of-the-art language model that can generate human-like text.</li>
+			<li>
+				Babbage: A language model that can answer questions and provide information on a wide range
+				of topics.
+			</li>
+			<li>
+				Curie: A language model that can generate text with a sense of curiosity and playfulness.
+			</li>
+			<li>Davinci: A language model that can perform translation and summarization tasks.</li>
+		</ul>
+	</section>
+	<section class="mb-3">
+		<label class="form-label" for="model">Model:</label>
+		<form id="form">
+			<div class="mb-3">
+				<select class="form-select" id="model" name="model">
+					<option disabled value="ada">Ada</option>
+					<option value="text-babbage-001">Babbage</option>
+					<option value="text-babbage-002">Babbage (Technical)</option>
+					<option value="text-curie-001">Curie</option>
+					<option selected value="text-davinci-002">Davinci</option>
+				</select>
+			</div>
 
-	<form id="form">
-		<label for="model">Model:</label>
-		<select id="model" name="model">
-			<option disabled value="ada">Ada</option>
-			<option value="text-babbage-001">Babbage</option>
-			<option value="text-babbage-002">Babbage (Technical)</option>
-			<option value="text-curie-001">Curie</option>
-			<option selected value="text-davinci-002">Davinci</option>
-		</select>
-		<br />
-		<label for="prompt">Decklist:</label>
-		<textarea id="prompt" name="prompt" rows="10" />
-		<br />
-		<button type="submit">Generate response</button>
-	</form>
+			<div class="mb-3">
+				<label class="form-label" for="prompt">Decklist:</label>
+				<textarea class="form-control" id="prompt" name="prompt" rows="10" />
+			</div>
 
-	<div id="response" style="white-space: pre-wrap; font-family: inherit" />
+			<button class="btn btn-primary" type="submit">Generate response</button>
+		</form>
+	</section>
+
+	<section class="mb-3" id="response" style="white-space: pre-wrap; font-family: inherit" />
 </main>

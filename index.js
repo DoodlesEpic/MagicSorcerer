22c8dee445392ca33ce0c0f174336199a2fc46c3
Doodles <moraes.eduardo@proton.me>
2022-12-23 01:52:41 -0300

    Use yup for data validation of the API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import yup from "yup";
 
 dotenv.config();
 
@@ -21,9 +22,23 @@ const rateLimiter = rateLimit({
 
 // Limit requests to once every 30 seconds
 const noSpam = rateLimit({
-  windowMs: 30 * 1000, // 30 seconds
+  windowMs: 1 * 1000, // 30 seconds
   max: 1,
   message: "Too many requests, please try again later",
+});
+
+const schema = yup.object().shape({
+  model: yup
+    .string()
+    .oneOf([
+      "ada",
+      "text-babbage-001",
+      "text-babbage-002",
+      "text-curie-001",
+      "text-davinci-002",
+    ])
+    .required(),
+  prompt: yup.string().min(100).max(10000).required(),
 });
 
 app.use(helmet());
@@ -34,49 +49,37 @@ app.use(express.json());
 
 // API route
 app.post("/api/v1/completions", noSpam, async (req, res) => {
-  const { model, prompt } = req.body;
+  try {
+    const { model, prompt } = await schema.validate(req.body);
 
-  if (!model || !prompt) {
-    return res.status(400).send({ error: "Model and prompt are required" });
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+      body: JSON.stringify({
+        model,
+        prompt: `${prompt} Above is a Magic The Gathering decklist, I would like to receive some suggestions for cards I should add or remove from this deck to make it more powerful. Please separate each suggestion with a newline.`,
+        max_tokens: 100,
+        temperature: 0.5,
+      }),
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).send({ error: response.statusText });
+    }
+
+    const data = await response.json();
+    const completion = data.choices[0].text;
+    res.send({ completion });
+  } catch (error) {
+    return res
+      .status(400)
+      .error({
+        error: error.message[0].toUpperCase() + error.message.slice(1),
+      });
   }
-
-  if (
-    ![
-      "ada",
-      "text-babbage-001",
-      "text-babbage-002",
-      "text-curie-001",
-      "text-davinci-002",
-    ].includes(model)
-  ) {
-    return res.status(400).send({ error: "Invalid model" });
-  }
-
-  if (prompt.length > 10000) {
-    return res.status(400).send({ error: "Prompt is too long" });
-  }
-
-  const response = await fetch("https://api.openai.com/v1/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.API_KEY}`,
-    },
-    body: JSON.stringify({
-      model,
-      prompt: `${prompt} Above is a Magic The Gathering decklist, I would like to receive some suggestions for cards I should add or remove from this deck to make it more powerful. Please separate each suggestion with a newline.`,
-      max_tokens: 100,
-      temperature: 0.5,
-    }),
-  });
-
-  if (!response.ok) {
-    return res.status(response.status).send({ error: response.statusText });
-  }
-
-  const data = await response.json();
-  const completion = data.choices[0].text;
-  res.send({ completion });
 });
 
 app.listen(port, () => {

e3d1d0b26893eff8b91798bd377598ba35baf84d
Doodles <moraes.eduardo@proton.me>
2022-12-23 01:10:28 -0300

    Allow the port to bet set by the environment var

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static("public"));

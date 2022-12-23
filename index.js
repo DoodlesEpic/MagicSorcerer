3be7fb2b1a22ee501094bed521ba5f838a7d06a9
Doodles <moraes.eduardo@proton.me>
2022-12-23 01:27:29 -0300

    Add helmet headers to the express backend

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 import cors from "cors";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());

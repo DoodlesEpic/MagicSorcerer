841f79e25c8a20cad90af99737662b2e81856018
Doodles <moraes.eduardo@proton.me>
2022-12-23 18:43:06 -0300

    Add title and description meta tags to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import '../app.scss';
 </script>
 
+<svelte:head>
+	<title>MTG Sorcerer</title>
+	<meta
+		name="description"
+		content="Find powerful MTG card suggestions from AI with MTG Sorcerer. Utilizing advanced machine learning algorithms, our AI helps you improve your Magic: The Gathering deck. Try it out now!"
+	/>
+</svelte:head>
+
 <nav class="navbar sticky-top bg-body shadow-sm">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="/">MTG Sorcerer</a>
